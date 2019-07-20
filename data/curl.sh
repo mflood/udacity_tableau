36f579b664d3b149b73572b8f810d14224813296
Matthew Flood <hermannwest@gmail.com>
2019-07-20 11:09:30 -0600

    Download data set

---
+++ data/curl.sh
@@ -0,0 +1,7 @@
+
+# Data Source:
+# https://www.cooldatasets.com/#Entertainment-Datasets
+
+URL=https://raw.githubusercontent.com/hopperrr/ellis-immigration-by-ship/gh-pages/data/trips.tsv
+
+curl -o trips.tsv $URL
